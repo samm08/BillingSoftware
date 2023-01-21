fc6ee873d4607f8c87604a5cab2a14aa213ad057
Sambhaji Mange <sambhaji@magtapp.in>
2023-01-21 15:11:55 +0530

    Initial Commit

---
+++ be- news software/utils/mongodb/indexDao.js
@@ -0,0 +1,14 @@
+const RegistrationModel = require('../mongodb/model/registration.model')
+
+const registerCompany = async(data) => {
+    console.log("data", data)
+    try {
+        
+    } catch (error) {
+        throw error;
+    }
+}
+
+module.exports = {
+    registerCompany,
+}
---
+++ be- news software/utils/mongodb/model/registration.model.js
@@ -0,0 +1,39 @@
+const {Schema, model } = require('mongoose')
+
+const schema = new Schema({
+    company_name : {
+        type: String,
+        required: true
+    },
+    contact_person : {
+        type: String,
+        required: true
+    },
+    contact_number : {
+        type: Number,
+        required: true
+    },
+    email_id : {
+        type: String,
+        required: true
+    },
+    password : {
+        type: String,
+        required: true
+    },
+    company_address : {
+        type: String,
+        required: true
+    },
+    gst_no : {
+        type: String,
+        required: false
+    },
+    registration_key : {
+        type: String,
+        required: true
+    }
+},{timestamps: true});
+
+const _model = model("company", schema);
+module.exports = _model
